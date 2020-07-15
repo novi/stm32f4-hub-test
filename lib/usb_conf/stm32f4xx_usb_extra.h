e79c015d63d6a7e0e0baa617f9daf34b44302507
Yusuke Ito <novi.mad@gmail.com>
2020-07-15 17:00:23 +0900

    fix build error 2

--- lib/usb_conf/stm32f4xx_usb_extra.h
+++ lib/usb_conf/stm32f4xx_usb_extra.h
@@ -1,7 +1,10 @@
-#ifndef __STM32F4xx_LL_USB_H
-#define __STM32F4xx_LL_USB_H
+#ifndef __STM32F4xx_USB_EXTRA_H
+#define __STM32F4xx_USB_EXTRA_H
 
 #include "stm32f4xx_hal.h"
 
 HAL_StatusTypeDef USB_StopHostChannel(USB_OTG_GlobalTypeDef *USBx, uint8_t chnum);
 HAL_StatusTypeDef HAL_HCD_StopHC(HCD_HandleTypeDef *hhcd, uint8_t chnum);
+
+#endif // #ifndef __STM32F4xx_LL_USB_H
+#define __STM32F4xx_USB_EXTRA_H
