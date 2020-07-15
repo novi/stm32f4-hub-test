db2b76e0c110cf09517551d494c307557c489174
Yusuke Ito <novi.mad@gmail.com>
2020-07-15 17:03:47 +0900

    cleanup

--- lib/usb_conf/stm32f4xx_usb_extra.h
+++ lib/usb_conf/stm32f4xx_usb_extra.h
@@ -1,10 +1,9 @@
-#ifndef __STM32F4xx_USB_EXTRA_H
-#define __STM32F4xx_USB_EXTRA_H
+#ifndef _STM32F4xx_USB_EXTRA_H
+#define _STM32F4xx_USB_EXTRA_H
 
 #include "stm32f4xx_hal.h"
 
 HAL_StatusTypeDef USB_StopHostChannel(USB_OTG_GlobalTypeDef *USBx, uint8_t chnum);
 HAL_StatusTypeDef HAL_HCD_StopHC(HCD_HandleTypeDef *hhcd, uint8_t chnum);
 
-#endif // #ifndef __STM32F4xx_LL_USB_H
-#define __STM32F4xx_USB_EXTRA_H
+#endif // #ifndef _STM32F4xx_USB_EXTRA_H
